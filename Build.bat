ce208fe1641dcfb36640fe5aa016eb1ce8ed74b7
The220th <The220th@gmail.com>
2021-05-04 13:13:13 +0300

    win

---
+++ Build.bat
@@ -0,0 +1,7 @@
+chcp 1251
+del /s *.class
+javac ./CipherLib/AES256.java
+javac ./CipherLib/RSA4096.java
+jar cvf0 ./CipherLib-Beta_V0.1.jar .\CipherLib\ByteWorker.class .\CipherLib\Tools.class .\CipherLib\IAsymCipher.class .\CipherLib\ISymCipher.class .\CipherLib\RSA4096.class .\CipherLib\AES256.class
+Pause
+CMD /Q /K
